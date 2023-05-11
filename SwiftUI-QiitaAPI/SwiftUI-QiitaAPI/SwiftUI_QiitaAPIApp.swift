6b65b17e2ab792257c99bef387eca1d53e2babfe
SoyaAsakura <s.asakura@arsaga.jp>
2023-05-11 09:41:00 +0900

    Xcodeプロジェクト作成

---
+++ SwiftUI-QiitaAPI/SwiftUI-QiitaAPI/SwiftUI_QiitaAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_QiitaAPIApp.swift
+//  SwiftUI-QiitaAPI
+//
+//  Created by s.asakura on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_QiitaAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
